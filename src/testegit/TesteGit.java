8f346ca2bd0dd711a0390e2fbde510c5c229e938
EuEMinhaCasa <EuEMinhaCasa@DESKTOP-0MKUQUE>
2018-01-13 02:33:06 -0200

    Iniciei o Git

---
+++ src/testegit/TesteGit.java
@@ -0,0 +1,10 @@
+package testegit;
+
+public class TesteGit {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Testando o Git");
+    }
+    
+}
