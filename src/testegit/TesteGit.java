b0c3ed5a1479bb77ba30ee4d393594c65161a435
EuEMinhaCasa <EuEMinhaCasa@DESKTOP-0MKUQUE>
2018-01-13 03:31:44 -0200

    Inseri um JOptionpane

--- src/testegit/TesteGit.java
+++ src/testegit/TesteGit.java
@@ -1,11 +1,14 @@
 package testegit;
 
+import javax.swing.JOptionPane;
+
 public class TesteGit {
 
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Testando o Git");
         System.out.println("Tomara que tudo tenha saido corretamente!");
+        JOptionPane.showMessageDialog(null, "Deu tudo certo");
     }
     
 }
